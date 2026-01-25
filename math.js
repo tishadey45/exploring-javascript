e4e958edac2ff7258a84feb834e08b0c790499d4
Tisha Dey <tishadey2025@outlook.com>
2026-01-26 00:46:28 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,29 @@
+// number
+var a = 10;
+var a = 7.7;
+// console.log(a, b);
+let a1 = 10;
+let b1 = 7.7;
+// console.log(a);
+
+// integer 
+let x =25;
+let y =-9;
+// console.log(x,y);
+
+// float
+let c = 34.53;
+let d = 279.23;
+// console.log(c, d);
+
+var price =35;
+//  price += 10;
+//  price + price =10;
+price = price + 10
+//  console.log(price);
+
+var fridge =450;
+// fridge -= 1000;
+fridge = fridge - 1000
+console.log(fridge);
+
