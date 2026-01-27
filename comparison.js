9cb675d29181a2c3d06d80d86a8401fd86af4dda
Tisha Dey <tishadey2025@outlook.com>
2026-01-27 19:30:32 +0600

    :memo: comparison done

---
+++ comparison.js
@@ -0,0 +1,19 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 ==10);
+// console.log(10 >= 10);
+// console.log(15 >= 10);
+// console.log(15 <= 10);
+// console.log(15 <= 15);
+
+// var orangePrice =220;
+// var mangoPrice =150;
+// console.log(orangePrice > mangoPrice);
+
+// a===5
+// a==3
+// a!=7
+
+// 15>=6
+
