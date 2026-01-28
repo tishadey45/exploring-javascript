f6c2e09f0acb0b190fbdb9825e128ac4e12b8485
Tisha Dey <tishadey2025@outlook.com>
2026-01-28 21:34:08 +0600

    :green_heart: nested done

--- if.js
+++ if.js
@@ -26,7 +26,6 @@
 //   console.log("kinbo na");
 // }
 
-
 // let marks = 60;
 // 100+ => Invalid Input
 // 100-80 => A+
@@ -38,20 +37,20 @@
 // 0-32=>F
 
 const marks = 110;
-if (marks >= 100){
-    console.log("Invalid Input");
-}else if(marks >=100 && marks<=80){
-    console.log("A+");
-}else if(marks >=70 && marks <=79){
-     console.log("A");
-}else if(marks>=60 && marks <=69){
-    console.log("A-");
-}else if(marks>=50 && marks <=59){
-    console.log("B");
-}else if(marks >= 40 && marks <= 49){
-    console.log("C");
-}else if(marks >= 30 && marks <=39){
-    console.log("D");
-}else if(marks >= 0 && marks <=32){
-    console.log("F");
+if (marks >= 100) {
+  console.log("Invalid Input");
+} else if (marks >= 100 && marks <= 80) {
+  console.log("A+");
+} else if (marks >= 70 && marks <= 79) {
+  console.log("A");
+} else if (marks >= 60 && marks <= 69) {
+  console.log("A-");
+} else if (marks >= 50 && marks <= 59) {
+  console.log("B");
+} else if (marks >= 40 && marks <= 49) {
+  console.log("C");
+} else if (marks >= 30 && marks <= 39) {
+  console.log("D");
+} else if (marks >= 0 && marks <= 32) {
+  console.log("F");
 }
---
+++ nested.js
@@ -0,0 +1,20 @@
+const money =20;
+if(money >=600){
+    console.log("probir tui onek rich");
+}
+else{
+    if(money >=300){
+     console.log("pizza o kina jay berger o kina jay");
+    }
+    else{
+      if(money >=100){
+        console.log("clod dinks kha");
+      }
+      else{
+       console.log("dur chata kicui khabo na");
+      }
+    }
+}
+
+
+// console.log("object");
