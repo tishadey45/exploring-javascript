a3bab43fa77c8b08b648d55611f1b231bc2e6ed9
Tisha Dey <tishadey2025@outlook.com>
2026-01-29 09:33:37 +0600

    :green_heart: array done

--- Array/array.js
+++ Array/array.js
@@ -1,4 +1,25 @@
-const home = ["father","mother","husband","ami"]
-// console.log(home [0]);
-// console.log(home [2]);
-console.log(home.length);
+const home = ["father", "mother", "husband", "ami"];
+
+// console.log(home.indexOf("tomato"));
+// console.log(home[2]);
+// console.log(home.length);
+
+// const number = [20, 12, 30, 92, 13, 34];
+// number.push =[40]// akta array te arekta array []
+// number.push(40);  // notun deta jog hoy
+// console.log(number);
+
+// number.pop ();// laster deta remove kore
+// console.log(number);
+
+// number.unshift (10); // surute data jog kore
+// console.log(number);
+
+// number.shift (); // surute data remove kore
+// console.log(number);
+
+
+// home.push ("tometo");
+// console.log(home);
+
+// console.log(home.includes("husband"));
