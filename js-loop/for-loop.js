463cfb2acf802284613af0237b847a061a40f406
Tisha Dey <tishadey2025@outlook.com>
2026-01-30 21:53:01 +0600

    :art: js loop done

--- js-loop/for-loop.js
+++ js-loop/for-loop.js
@@ -6,50 +6,44 @@
 //   console.log(i);
 // }
 
-
-
-
 // for (let index =30; index > 20; index--){
 //     console.log(index);
 // }
-
-
-
-
-
-
-
 
 // for (let num=50; num > 30; num--){
 //     console.log(num);
 // }
 
+// for (let x = 1000; x <= 100000; x = x + 5000) {
+//   console.log(x);
+// }
+
+// for (let i =50; i<100; i++){
+//     console.log(i);
+// }
+
+// for (let num =150; num<=170; num++){
+//     console.log(num);
+// }
 
 
+// let sum =0
+// for (let a =31; a <= 58; a++){
+//     sum=sum+ a;
+// }
+// console.log("sum of numbers 31 to 58 is:",sum);
 
+// jor loop
+// for (let num =0; num < 20; num++){
+//     if(num % 2 == 0){
+//         console.log(num);
+//     }
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-for (let x = 1000; x <= 100000; x=x+5000){
-    console.log(x);
-}
-
-
-
-
-
-
-
-
-
-
+// bejor loop
+// for (let x = 0; x < 30; x++){
+//     if (x % 2 !== 0){
+//         console.log(x);
+//     }
+// }
 
