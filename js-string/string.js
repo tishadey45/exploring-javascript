a6ba201d5003d70c4e0884302911b3dcb6f8c0e2
Tisha Dey <tishadey2025@outlook.com>
2026-01-31 23:40:49 +0600

    :tada: string almost done

---
+++ js-string/string.js
@@ -0,0 +1,20 @@
+const country = "bangladesh";
+const x = 'mango';
+const a = `hore-krishno`;
+const y = new String('moniram');
+
+// console.log(typeof y);
+// console.log(y);
+
+const numbers = [25,14,45,34,56];
+// console.log(numbers.length);
+// console.log(numbers[2]);
+
+const num = "moniram";//koyta jayga dokhol kore
+// console.log(num.length);
+// console.log(num[1]);
+
+
+let school = "Moth Khola Sorkari School"
+let book = school.toLowerCase();
+console.log(book);
